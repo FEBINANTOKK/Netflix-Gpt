ea528c0cfac5e7b366709c4f6f1e935302218d86
unknown <febinanto61@gmail.com>
2024-09-04 22:51:28 +0530

    Set the background trial movie video in fullscreen and autoplay it with muted audio

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -10,12 +10,14 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
+        className="w-screen aspect-video"
         src={
-          "https://www.youtube.com/embed/u69y5Ie519M?si=" + trailerVideo?.key
+          "https://www.youtube.com/embed/u69y5Ie519M?si=" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
         }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </div>
   );
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className=" pt-52 pl-14">
+    <div className=" pt-52 pl-14 absolute w-screen aspect-video bg-black bg-opacity-50 text-white cursor-pointer ">
       <h3 className="text-3xl font-bold pb-4"> {title}</h3>
       <p className=" font-light text-xs max-w-[30%] mb-5">{overview}</p>
 
       <div>
-        <button className="bg-black  text-white rounded-lg px-7 py-1 bg-opacity-80">
-          Play
+        <button className="bg-white text-black rounded-lg px-7 py-1 hover:bg-opacity-80">
+          ▷ Play
         </button>
-        <button className="bg-black  text-white rounded-lg px-7 py-1 ml-4 bg-opacity-80">
+        <button className="bg-gray-500  rounded-lg px-7 py-1 ml-4 ">
           More Info
         </button>
       </div>
